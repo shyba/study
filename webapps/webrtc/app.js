29cae98d0c6d7321067a33de1cb53d58ff988edd
Victor Shyba <victor.shyba@gmail.com>
2022-02-05 00:48:18 -0300

    use trackers, dont use http sources

--- webapps/webrtc/app.js
+++ webapps/webrtc/app.js
@@ -1,11 +1,27 @@
 var WebTorrent = require('webtorrent')
 
-var client = new WebTorrent()
+var trackers = ['wss://tracker.btorrent.xyz', 'wss://tracker.openwebtorrent.com']
+
+var rtcConfig = {
+  'iceServers': [
+    {
+      'urls': ['stun:stun.l.google.com:19305', 'stun:stun1.l.google.com:19305']
+    }
+  ]
+}
+
+var trackerOpts = {
+  announce: trackers,
+  rtcConfig: rtcConfig
+}
+
+var client = new WebTorrent({tracker: trackerOpts})
+
 
 // Sintel, a free, Creative Commons movie
-var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10'
 
-client.add(torrentId, function (torrent) {
+client.add(torrentId, {announce: trackers}, function (torrent) {
   // Torrents can contain many files. Let's use the .mp4 file
   var file = torrent.files.find(function (file) {
     return file.name.endsWith('.mp4')
