ac8dad1be57c9eb9ae93b5f5fbb9b4c36c644d82
Victor Shyba <victor.shyba@gmail.com>
2022-05-11 17:10:35 -0300

    n2t assembler: file lister

--- nand2tetris/assembler/src/main.rs
+++ nand2tetris/assembler/src/main.rs
@@ -1,3 +1,38 @@
+extern crate core;
+
+use std::{env, fs, io};
+use std::path::{Path, PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let files = match env::args().len() {
+        x if x == 1 => list_asm_files(),
+        _ => args_to_files(),
+    };
+    if files.expect("Error reading directory/files.").len() == 0 {
+        println!("Nothing to assemble!");
+        return;
+    }
+}
+
+fn list_asm_files() -> io::Result<Vec<PathBuf>> {
+    let mut files: Vec<PathBuf> = vec![];
+    for entry in fs::read_dir(Path::new("."))? {
+        let entry = entry?;
+        match entry.path() {
+            p if p.ends_with("asm") => files.push(entry.path()),
+            _ => continue
+        }
+    }
+    Ok(files)
+}
+
+fn args_to_files() -> io::Result<Vec<PathBuf>> {
+    let mut files: Vec<PathBuf> = vec![];
+    for name in env::args().skip(1) {
+        match Path::new(&name) {
+            p if p.exists() && !p.is_dir() => files.push(p.to_path_buf()),
+            _ => continue
+        }
+    }
+    Ok(files)
 }
