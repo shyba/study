20d96aec8fbc0acecef7e877828326d206be922b
Victor Shyba <victor.shyba@gmail.com>
2022-05-17 00:50:52 -0300

    count neighbors and test

--- rust/game-of-life/gol-rs/src/gol.rs
+++ rust/game-of-life/gol-rs/src/gol.rs
@@ -1,5 +1,6 @@
 use bitvec::prelude as bv;
 use core::str::FromStr;
+use core::cmp::min;
 
 pub const COLUMNS: usize = 64;
 pub const ROWS: usize = 20;
@@ -33,7 +34,7 @@ impl GameOfLife {
         GameOfLife {screen: bv::bitarr!(u8, bv::Msb0; 0; COLUMNS*ROWS)}
     }
 
-    pub fn get_at(self, row: usize, col: usize) -> Option<bool> {
+    pub fn get_at(self: &Self, row: usize, col: usize) -> Option<bool> {
         let row = row * ROWS;
         match self.screen.get(row + col) {
             Some(x) if *x => Some(true),
@@ -45,5 +46,40 @@ impl GameOfLife {
     pub fn advance(self: &mut Self) {
         let tmp = GameOfLife::new();
         self.screen = tmp.screen;
+    }
+
+    pub fn count_alive_neighbors(self: &Self, row: usize, col: usize) -> usize {
+        let min_row = row.checked_sub(1).unwrap_or(0);
+        let max_row = min(ROWS-1, row+1);
+        let min_col = col.checked_sub(1).unwrap_or(0);
+        let max_col = min(COLUMNS-1, col+1);
+        let mut alive = 0;
+        for idxr in min_row..=max_row {
+            for idxc in min_col..=max_col {
+                if idxc == col && idxr == row {
+                    continue
+                }
+                alive = match self.get_at(idxr, idxc) {
+                    Some(true) => alive + 1,
+                    _ => alive
+                };
+            }
+        }
+        alive
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::gol::GameOfLife;
+    use core::str::FromStr;
+    #[test]
+    fn it_counts_alive_neighbors() {
+        let game = GameOfLife::from_str("#\n#\n#\n###").unwrap();
+        assert_eq!(1, game.count_alive_neighbors(0, 0));
+        assert_eq!(2, game.count_alive_neighbors(1, 0));
+        assert_eq!(3, game.count_alive_neighbors(2, 0));
+        assert_eq!(5, game.count_alive_neighbors(2, 1));
+        assert_eq!(2, game.count_alive_neighbors(3, 0));
     }
 }
