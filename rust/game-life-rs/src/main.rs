e40d28253733567c670f8fce66fb243b551d7475
Victor Shyba <victor.shyba@gmail.com>
2022-05-16 21:58:50 -0300

    gol: structs and render

--- rust/game-life-rs/src/main.rs
+++ rust/game-life-rs/src/main.rs
@@ -1,3 +1,35 @@
+use bitvec::prelude as bv;
+
+const COLUMNS: usize = 64;
+const ROWS: usize = 20;
+pub struct GameOfLife {
+    screen: bv::BitArr!(for COLUMNS*ROWS, in u8, bv::Msb0)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let game = GameOfLife::new();
+    render(&game);
+}
+
+fn render(game: &GameOfLife) {
+    println!("{}", "-".repeat(COLUMNS+2));
+    for idxr in 0..ROWS {
+        print!("|");
+        for idxc in 0..COLUMNS {
+            let index = idxr*COLUMNS + idxc;
+            match game.screen.get(index).as_deref() {
+                Some(true) => print!("#"),
+                Some(false) => print!(" "),
+                None => println!("ERROR AT {}", index)
+            }
+        }
+        println!("|");
+    }
+    println!("{}", "-".repeat(COLUMNS+2));
+}
+
+impl GameOfLife {
+    pub fn new() -> GameOfLife {
+        GameOfLife {screen: bv::bitarr!(u8, bv::Msb0; 0; COLUMNS*ROWS)}
+    }
 }
