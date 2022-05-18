955981ac92dd528ca00cefb60dae40948050f886
Victor Shyba <victor.shyba@gmail.com>
2022-05-18 09:32:39 -0300

    join lib tests in a single place

--- rust/game-of-life/gol-rs/src/gol.rs
+++ rust/game-of-life/gol-rs/src/gol.rs
@@ -91,6 +91,78 @@ mod tests {
     use crate::gol::GameOfLife;
     use core::str::FromStr;
     #[test]
+    fn it_can_parse() {
+        let game = GameOfLife::from_str(
+            "### #\n\
+             #\n\
+             #").unwrap();
+        assert_eq!(true, game.screen.get(0..3).unwrap().all());
+        assert_eq!(false, game.screen.get(0..4).unwrap().all());
+        assert_eq!(true, game.screen.get(4).unwrap());
+
+        assert_eq!(true, game.get_at(1, 0).unwrap());
+    }
+
+    #[test]
+    fn it_dies_from_underpop_simple() {
+        let mut game = GameOfLife::from_str("##").unwrap();
+        game.advance();
+        let expected = GameOfLife::from_str("  ").unwrap();
+        assert_eq!(expected.screen, game.screen);
+    }
+
+    #[test]
+    fn it_revives_from_three_cells() {
+        let mut game = GameOfLife::from_str(r#"
+        #
+        #
+          #
+        "#).unwrap();
+        game.advance();
+        let expected = GameOfLife::from_str(r#"
+
+         #
+
+        "#).unwrap();
+        assert_eq!(expected.screen, game.screen);
+    }
+
+    #[test]
+    fn it_survives() {
+        let mut game = GameOfLife::from_str(r#"
+            #
+             #
+             #
+        "#
+        ).unwrap();
+        game.advance();
+        let expected = GameOfLife::from_str(r#"
+
+            ##
+
+        "#
+        ).unwrap();
+        assert_eq!(expected.screen, game.screen);
+    }
+
+    #[test]
+    fn it_dies_from_overpop() {
+        let mut game = GameOfLife::from_str(r#"
+            # #
+            ##
+             ##
+        "#
+        ).unwrap();
+        game.advance();
+        let expected = GameOfLife::from_str(r#"
+            #
+            #
+            ###
+        "#
+        ).unwrap();
+        assert_eq!(expected.screen, game.screen);
+    }
+    #[test]
     fn it_counts_alive_neighbors() {
         let game = GameOfLife::from_str("#\n#\n#\n###").unwrap();
         assert_eq!(1, game.count_alive_neighbors(0, 0));
