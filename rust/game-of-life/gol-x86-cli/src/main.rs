a6b31ba72aca7448333bf533371430d6c3aee365
Victor Shyba <victor.shyba@gmail.com>
2022-05-30 19:09:22 -0300

    fix gol x86

--- rust/game-of-life/gol-x86-cli/src/main.rs
+++ rust/game-of-life/gol-x86-cli/src/main.rs
@@ -1,17 +1,17 @@
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
+use gol_rs::gol::*;
 use std::cmp::min;
 use std::io;
-use gol_rs::gol::*;
 use std::str::FromStr;
 use std::thread::sleep;
 use std::time::Duration;
-use crossterm::execute;
-use crossterm::terminal::{disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
 use tui::backend::CrosstermBackend;
 use tui::buffer::Buffer;
 use tui::layout::Rect;
 use tui::style::Color;
-use tui::Terminal;
 use tui::widgets::Widget;
+use tui::Terminal;
 
 fn main() {
     let mut game = GameOfLife::from_str(
@@ -20,7 +20,9 @@ fn main() {
 
                 #
                ###
-                #"#).unwrap();
+                #"#,
+    )
+    .unwrap();
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen).unwrap();
     let backend = CrosstermBackend::new(stdout);
@@ -29,11 +31,13 @@ fn main() {
     let tick_rate = Duration::from_millis(100);
 
     for _ in 0..100 {
-        terminal.draw(|f| {
-            let size = f.size();
-            let widget = App::new(&game);
-            f.render_widget(widget, size);
-        }).unwrap();
+        terminal
+            .draw(|f| {
+                let size = f.size();
+                let widget = App::new(&game);
+                f.render_widget(widget, size);
+            })
+            .unwrap();
         sleep(tick_rate);
         game.advance();
     }
@@ -48,9 +52,7 @@ struct App<'a> {
 
 impl<'a> App<'a> {
     fn new(game: &'a GameOfLife) -> App {
-        App {
-            game,
-        }
+        App { game }
     }
 
     fn all_black(self: &Self, buf: &mut Buffer) {
@@ -61,8 +63,17 @@ impl<'a> App<'a> {
 
     fn game_to_buf(self, area: Rect, buf: &mut Buffer) {
         //self.all_black(buf);
-        let cols = min(COLUMNS, area.width.checked_sub(COLUMNS as u16).unwrap_or(0u16).into());
-        let rows = min(ROWS, area.height.checked_sub(ROWS as u16).unwrap_or(0u16).into());
+        let cols = min(
+            COLUMNS,
+            area.width
+                .checked_sub(COLUMNS as u16)
+                .unwrap_or(20u16)
+                .into(),
+        );
+        let rows = min(
+            ROWS,
+            area.height.checked_sub(ROWS as u16).unwrap_or(20u16).into(),
+        );
         for idxr in 0..rows {
             for idxc in 0..cols {
                 let value = self.game.get_at(idxr, idxc).unwrap();
@@ -70,7 +81,7 @@ impl<'a> App<'a> {
                     true => Color::Red,
                     _ => Color::Black,
                 };
-                buf.get_mut(idxc as u16, (10+idxr) as u16).set_bg(color);
+                buf.get_mut(idxc as u16, (10 + idxr) as u16).set_bg(color);
             }
         }
     }
@@ -86,19 +97,19 @@ impl<'a> Widget for App<'a> {
 fn render(game: &GameOfLife) {
     dbg!(game.screen.count_ones());
     dbg!(game.screen.count_zeros());
-    println!("{}", "-".repeat(COLUMNS+2));
+    println!("{}", "-".repeat(COLUMNS + 2));
     for idxr in 0..ROWS {
         print!("|");
         for idxc in 0..COLUMNS {
-            let index = idxr*COLUMNS + idxc;
+            let index = idxr * COLUMNS + idxc;
             let value = game.screen.get(index);
             match value.as_deref() {
                 Some(true) => print!("#"),
                 Some(false) => print!("-"),
-                None => println!("ERROR AT {}", index)
+                None => println!("ERROR AT {}", index),
             }
         }
         println!("|");
     }
-    println!("{}", "-".repeat(COLUMNS+2));
+    println!("{}", "-".repeat(COLUMNS + 2));
 }
