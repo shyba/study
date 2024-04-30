0c2c0cd0071049c76e63399a3178029db8f1ad24
Victor Shyba <victor.shyba@gmail.com>
2024-04-30 16:15:37 -0300

    add furious sleep

--- rust/sleepsort/src/bin/simple.py
+++ rust/sleepsort/src/bin/simple.py
@@ -1,3 +1,4 @@
+import time
 import asyncio
 from typing import List, Callable
 
@@ -17,5 +18,20 @@ async def main():
     print(f"sorted: {await sort(original)}")
     print(f"sorted: {await sort_broken(original)}")
 
+async def sleep_furiously():
+    start = time.time()
+    loop = asyncio.get_event_loop()
+    original_clock = loop.time
+    drift = 0
+    loop.time = lambda: original_clock() + drift
+    original_data = [10, 0, 2, 6, 100_000_000]
+    pending = asyncio.ensure_future(sort(original_data))
+    while not pending.done():
+        drift += 1
+        await asyncio.sleep(0)
+    print(await pending)
+    print("Took %.2f seconds in this universe." % (time.time()-start))
+
 if __name__ == '__main__':
     asyncio.run(main())
+    asyncio.run(sleep_furiously())
